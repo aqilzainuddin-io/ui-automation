e8f6863dbfe4d99c0167f9c24ec9059af73c1c52
aqilzainuddin <aqilzainuddin.io@gmail.com>
2025-10-07 23:04:30 +0800

    feat: add envreader

---
+++ src/test/java/utils/EnvReader.java
@@ -0,0 +1,14 @@
+package utils;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnvReader {
+    private static final Dotenv dotenv = Dotenv.configure()
+            .directory("./")  // root directory
+            .ignoreIfMissing()
+            .load();
+
+    public static String get(String key) {
+        return dotenv.get(key);
+    }
+}
