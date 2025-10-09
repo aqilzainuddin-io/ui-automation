b735b8c5acf3dba46691fee398266b5d3d2c3892
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2025-10-09 10:12:10 +0800

    feat: add common used method

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -3,6 +3,8 @@ package pages;
 import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.NoSuchElementException;
 
@@ -15,6 +17,9 @@ public class BasePage {
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
+    
+    // ===== Common Actions =====
+
     protected boolean isElementDisplayed(By locator) {
         try {
             return driver.findElement(locator).isDisplayed();
@@ -22,4 +27,43 @@ public class BasePage {
             return false;
         }
     }
+
+    public String getText(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).getText();
+    }
+
+    public void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+
+    public void type(By locator, String text) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        element.clear();
+        element.sendKeys(text);
+    }
+
+
+    // ===== Helper Methods =====
+    
+    public void waitForElementVisible(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void waitForElementClickable(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public void navigateTo(String url) {
+        driver.get(url);
+    }
+
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    public String getPageTitle() {
+        return driver.getTitle();
+    }
+
+
 }
