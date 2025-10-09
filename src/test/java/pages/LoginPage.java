88b315f8dea73d844a48e586ae0e003d8bbb30b5
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2025-10-09 10:13:57 +0800

    refactor: simplified the code

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,34 +1,30 @@
 package pages;
 
-import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LoginPage {
-    private WebDriver driver;
-    private WebDriverWait wait;
+public class LoginPage extends BasePage {
 
+    // ===== Locators =====
     private By usernameField = By.name("username");
     private By passwordField = By.name("password");
     private By loginButton = By.cssSelector("button[type='submit']");
     private By errorMessage = By.cssSelector(".oxd-alert-content-text");
 
+    // ===== Constructor =====
     public LoginPage(WebDriver driver) {
-        this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        super(driver);
     }
 
+    // ===== Actions =====
     public void login(String username, String password) {
-        driver.findElement(usernameField).clear();
-        driver.findElement(passwordField).clear();
-        driver.findElement(usernameField).sendKeys(username);
-        driver.findElement(passwordField).sendKeys(password);
-        driver.findElement(loginButton).click();
+        type(usernameField, username);
+        type(passwordField, password);
+        click(loginButton);
     }
 
+    // ===== Getters =====
     public String getErrorMessage() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage)).getText();
+        return getText(errorMessage);
     }
 }
