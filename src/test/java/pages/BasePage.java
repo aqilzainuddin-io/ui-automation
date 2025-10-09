5b1c0ec709e346b29193ed1c7e8a419efcc89fa1
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2025-10-09 08:49:42 +0800

    feat: add basepage class

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,25 @@
+package pages;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.NoSuchElementException;
+
+public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    protected boolean isElementDisplayed(By locator) {
+        try {
+            return driver.findElement(locator).isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+}
