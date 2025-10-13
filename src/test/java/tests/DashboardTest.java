c03dddf6c0d959aa8e440fdf88e29d59387b8781
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2025-10-13 19:15:09 +0800

    feat: add verify all nav link test case

--- src/test/java/tests/DashboardTest.java
+++ src/test/java/tests/DashboardTest.java
@@ -27,7 +27,10 @@ public class DashboardTest extends BaseTest {
         // Step 2: Assert Dashboard loaded
         Assert.assertTrue(dashboardPage.isDashboardDisplayed(), "Dashboard header not visible!");
 
-        // Step 3: Check important widgets
+        // Step 3: Title check
+        System.out.println("Dashboard title: " + dashboardPage.getDashboardTitle());
+
+        // Step 4: Verify all widgets visible
         Assert.assertTrue(dashboardPage.isQuickLaunchVisible(), "Quick Launch widget missing!");
         Assert.assertTrue(dashboardPage.isTimeAtWorkVisible(), "Time at Work widget missing!");
         Assert.assertTrue(dashboardPage.isMyActionsVisible(), "My Actions widget missing!");
@@ -36,7 +39,18 @@ public class DashboardTest extends BaseTest {
         Assert.assertTrue(dashboardPage.isEmployeeDistributionBySubUnitVisible(), "Employee Distribution By Unit missing!");
         Assert.assertTrue(dashboardPage.isEmployeeDistributionByLocationVisible(), "Employee Distribution By Location widget missing!");
 
-        // Step 4: Title check
-        System.out.println("Dashboard title: " + dashboardPage.getDashboardTitle());
+        // Step 5: Verify all navigation link visible
+        Assert.assertTrue(dashboardPage.isAdminPageVisible(),"Admin navigation link missing!");
+        Assert.assertTrue(dashboardPage.isPIMPageVisible(),"PIM navigation link missing!");
+        Assert.assertTrue(dashboardPage.isLeavePageVisible(),"Leave navigation link missing!");
+        Assert.assertTrue(dashboardPage.isTimePageVisible(),"Time navigation link missing!");
+        Assert.assertTrue(dashboardPage.isRecruitmentPageVisible(),"Recruitment navigation link missing!");
+        Assert.assertTrue(dashboardPage.isMyInfoPageVisible(),"My Info navigation link missing!");
+        Assert.assertTrue(dashboardPage.isPerformancePageVisible(),"Performance navigation link missing!");
+        Assert.assertTrue(dashboardPage.isDashboardPageVisible(),"Dashboard navigation link missing!");
+        Assert.assertTrue(dashboardPage.isDirectoryPageVisible(),"Directory navigation link missing!");
+        Assert.assertTrue(dashboardPage.isMaintenancePageVisible(),"Maintenance navigation link missing!");
+        Assert.assertTrue(dashboardPage.isClaimPageVisible(),"Claim navigation link missing!");
+        Assert.assertTrue(dashboardPage.isBuzzPageVisible(),"Buzz navigation link missing!");
     }
 }
