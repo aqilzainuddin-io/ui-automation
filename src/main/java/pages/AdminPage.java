57bf87080e177b6c7394f0fe76247ca0981183cd
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2025-10-21 17:15:45 +0800

    feat: fix pipeline and add locators adminpage

--- src/main/java/pages/AdminPage.java
+++ src/main/java/pages/AdminPage.java
@@ -3,26 +3,37 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import com.github.dockerjava.core.dockerfile.DockerfileStatement.Add;
+import com.google.common.collect.Table;
+
 public class AdminPage extends BasePage {
     
-    // locators
+    // verify admin/user management page
     private By adminHeader = By.cssSelector("oxd-topbar-header-breadcrumb-module");
     private By systemUsers = By.cssSelector("oxd-table-filter-title");
     private By recordsFound = By.xpath("//span[contains(normalize-space(),'Records Found')]");
     
+    // 
     private By usernameLbl = By.xpath("//label[contains(normalize-space(),'Username')]");
+    private By usernameInput = By.xpath("//label[text()='Username']/../following-sibling::div//input");
+    
     private By userRoleLbl = By.xpath("//label[contains(normalize-space(),'User Role')]");
+    private By userRoleInput = By.xpath("//label[text()='User Role']/../following-sibling::div//div[contains(@class,'oxd-select-text-input')]");
+
     private By employeeNameLbl = By.xpath("//label[contains(normalize-space(),'Employee Name')]");
-    private By statusLbl = By.xpath("//label[contains(normalize-space(),'Status')]");
-    
+    private By employeeNameInput = By.xpath("//label[text()='Employee Name']/../following-sibling::div//input");
 
-    private By usernameInput = By.xpath("//label[normalize-space()='Username']/ancestor::div[contains(@class,'oxd-input-group')]//input");
-    
+    private By statusLbl = By.xpath("//label[text()='Status']");
+    private By statusInput = By.xpath("//label[text()='Status']/../following-sibling::div//div[contains(@class,'oxd-select-text-input')]");
 
-    //input
+    private By searchBtn = By.xpath("//button[normalize-space()='Search']");
+    private By resetBtn = By.xpath("//button[normalize-space()='Reset']");
+    private By addBtn = By.xpath("//button[normalize-space()='Add']");
 
     // constructor
-    public LoginPage(WebDriver driver) {
+    public AdminPage(WebDriver driver) {
         super(driver);
     }
+
+
 }
