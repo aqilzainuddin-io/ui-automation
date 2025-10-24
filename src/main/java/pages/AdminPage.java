858f6752ecf01983955a50b51dbebc307b3f7bd3
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2025-10-25 05:43:23 +0800

    feat: fix locators and add methods

--- src/main/java/pages/AdminPage.java
+++ src/main/java/pages/AdminPage.java
@@ -5,24 +5,23 @@ import org.openqa.selenium.WebDriver;
 
 public class AdminPage extends BasePage {
     
-    // verify admin/user management page
-    private By adminHeader = By.cssSelector("oxd-topbar-header-breadcrumb-module");
-    private By systemUsers = By.cssSelector("oxd-table-filter-title");
-    private By recordsFound = By.xpath("//span[contains(normalize-space(),'Records Found')]");
+    // locators
+    private By adminHeader = By.xpath("//h6[contains(normalize-space(),'Admin')]");
+    private By jobBtn = By.xpath("//span[contains(normalize-space(),'Job')]");
+    private By jobTitlesLink = By.xpath("//a[contains(normalize-space(),'Job Titles')]");
+    private By employmentStatusLink = By.xpath("//a[contains(normalize-space(),'Employment Status')]");
+
+    private By systemUsersLbl = By.xpath("//h5[contains(normalize-space(),'System Users')]");
+    private By recordsFound = By.xpath("//span[contains(normalize-space(),'Records Found')]");                                  
     
-    // 
     private By usernameLbl = By.xpath("//label[contains(normalize-space(),'Username')]");
     private By usernameInput = By.xpath("//label[text()='Username']/../following-sibling::div//input");
-    
     private By userRoleLbl = By.xpath("//label[contains(normalize-space(),'User Role')]");
     private By userRoleInput = By.xpath("//label[text()='User Role']/../following-sibling::div//div[contains(@class,'oxd-select-text-input')]");
-
     private By employeeNameLbl = By.xpath("//label[contains(normalize-space(),'Employee Name')]");
     private By employeeNameInput = By.xpath("//label[text()='Employee Name']/../following-sibling::div//input");
-
     private By statusLbl = By.xpath("//label[text()='Status']");
     private By statusInput = By.xpath("//label[text()='Status']/../following-sibling::div//div[contains(@class,'oxd-select-text-input')]");
-
     private By searchBtn = By.xpath("//button[normalize-space()='Search']");
     private By resetBtn = By.xpath("//button[normalize-space()='Reset']");
     private By addBtn = By.xpath("//button[normalize-space()='Add']");
@@ -30,6 +29,16 @@ public class AdminPage extends BasePage {
     // constructor
     public AdminPage(WebDriver driver) {
         super(driver);
+    }
+
+    public boolean isAdminHeaderDisplayed(){
+        return isElementDisplayed(adminHeader); 
+    }
+    public String getAdminHeaderTitle(){
+        return getText(adminHeader);
+    }
+    public void clickAddUserBtn(){
+        click(addBtn);
     }
 
 }
