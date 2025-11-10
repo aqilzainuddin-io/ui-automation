12aabed518e1b86dc46be1184842bb6c5f19437f
aqilzainuddin-writer <aqilzainuddin.io@gmail.com>
2025-11-10 15:47:10 +0800

    feat: add methods

--- src/main/java/pages/login/LoginPage.java
+++ src/main/java/pages/login/LoginPage.java
@@ -8,14 +8,37 @@ import pages.BasePage;
 public class LoginPage extends BasePage {
 
     // locator
-    private By usernameField = By.name("username");
-    private By passwordField = By.name("password");
-    private By loginButton = By.cssSelector("button[type='submit']");
-    private By errorMessage = By.cssSelector(".oxd-alert-content-text");
+    private By usernameField = By.xpath("//label[text()='Username']/../following-sibling::div/input");
+    private By passwordField = By.xpath("//label[text()='Password']/../following-sibling::div/input");
+    private By loginButton = By.xpath("//button[contains(normalize-space(),'Login')]");
+    private By invalidCredMsg = By.xpath("//p[contains(normalize-space(),'Invalid credentials')]");
+    private By usernameRequiredMsg = By.xpath("//label[text()='Username']/../following-sibling::span[text()='Required']");
+    private By passwordRequiredMsg = By.xpath("//label[text()='Password']/../following-sibling::span[text()='Required']");
+
 
     // constructor
     public LoginPage(WebDriver driver) {
         super(driver);
+    }
+
+    // getter method
+    public boolean isInvalidCredErrMsgVisible() {
+        return isElementDisplayed(invalidCredMsg);
+    }
+    public String getInvalidCredErrMsg() {
+        return getText(invalidCredMsg);
+    }
+    public boolean isUsernameReqErrMsgVisible(){
+        return isElementDisplayed(usernameRequiredMsg);
+    }
+    public boolean isPasswordReqErrMsgVisible(){
+        return isElementDisplayed(passwordRequiredMsg);
+    }
+    public String getUsernameReqMsg(){
+        return getText(usernameRequiredMsg);
+    }
+    public String getPasswordReqMsg(){
+        return getText(passwordRequiredMsg);
     }
 
     // action method
@@ -25,12 +48,4 @@ public class LoginPage extends BasePage {
         click(loginButton);
     }
 
-    // getter method
-    public boolean isErrorMessageVisible() {
-        return isElementDisplayed(errorMessage);
-    }
-
-    public String getErrorMessage() {
-        return getText(errorMessage);
-    }
 }
